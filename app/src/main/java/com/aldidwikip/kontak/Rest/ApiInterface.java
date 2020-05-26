bc120d505dd4d17006bbf0c7ac1f3333db2c3181
aldidwikip <aldidwiki@gmail.com>
2020-05-26 16:19:36 +0700

    update api client

--- app/src/main/java/com/aldidwikip/kontak/Rest/ApiInterface.java
+++ app/src/main/java/com/aldidwikip/kontak/Rest/ApiInterface.java
@@ -15,22 +15,22 @@ import retrofit2.http.PUT;
 import retrofit2.http.Part;
 
 public interface ApiInterface {
-    @GET("Kontak_android")
+    @GET("kontak_android")
     Call<GetKontak> getKontak();
 
     @Multipart
-    @POST("Kontak")
+    @POST("kontak")
     Call<PostPutDelKontak> uploadFile(@Part MultipartBody.Part file);
 
     @FormUrlEncoded
-    @POST("Kontak")
+    @POST("kontak")
     Call<PostPutDelKontak> postKontak(@Field("nama") String nama,
                                       @Field("nomor") String nomor,
                                       @Field("alamat") String alamat,
                                       @Field("avatar") String avatar);
 
     @FormUrlEncoded
-    @PUT("Kontak")
+    @PUT("kontak")
     Call<PostPutDelKontak> putKontak(@Field("id") String id,
                                      @Field("nama") String nama,
                                      @Field("nomor") String nomor,
@@ -38,8 +38,7 @@ public interface ApiInterface {
                                      @Field("avatar") String avatar);
 
     @FormUrlEncoded
-    @HTTP(method = "DELETE", path = "Kontak", hasBody = true)
+    @HTTP(method = "DELETE", path = "kontak", hasBody = true)
     Call<PostPutDelKontak> deleteKontak(@Field("id") String id);
-
 
 }
